6e2df95d4032aa3ff983d8aa292dc07d5fa0fd47
siegerts <stephen.siegert@gmail.com>
2021-05-24 12:00:35 -0400

    initial implementation

---
+++ amplify/backend/function/githubdailyupdatecb16306e/src/index.py
@@ -0,0 +1,343 @@
+import os
+import json
+from datetime import date, datetime, timedelta
+import sys
+from typing import List
+
+import boto3
+import requests
+
+ENV = os.environ.get("ENV", None)
+PROD = ENV == "dev"
+
+if PROD:
+    from aws_lambda_powertools import Logger
+    logging = Logger(level="INFO", service="github-updates")
+
+    REGION = os.environ["REGION"]
+    REPO_TABLE_NAME = os.environ["STORAGE_DYNAMO41B205C8_NAME"]
+    TOKEN = os.environ.get("GH_TOKEN")
+
+    dynamodb = boto3.resource("dynamodb", region_name=REGION)
+    repo_table = dynamodb.Table(REPO_TABLE_NAME)
+
+    repos = repo_table.scan()['Items']
+
+
+else:
+    import logging
+    logging.basicConfig(format='%(levelname)s:%(message)s',
+                        level=logging.DEBUG)
+
+    from dotenv import load_dotenv
+    load_dotenv()
+    MEMBERS = json.loads(os.environ['MEMBERS'])
+    WEBHOOK = os.environ.get("TEST_WEBHOOK")
+    TOKEN = os.environ.get("TOKEN")
+
+    # local mock
+    repos = [{
+        "id": "amplify-cli",
+        "repo": "amplify-cli",
+        "team": "amplify-cli",
+        "webhook": WEBHOOK,
+        "members": MEMBERS,
+        "name": "Amplify CLI",
+        "updated_members_at": "2021-05-20T22:25:52.409Z"
+    }]
+
+
+GITHUB_BASE_URL = "https://api.github.com"
+HEADERS = {"Accept": "application/vnd.github.v3+json",
+           "Authorization": "token " + TOKEN}
+
+
+FILTER_LABELS = set(["feature-request", "enhancement",
+                     "bug", "pending-close-response-required"])
+
+TIME_INTERVAL = 4
+TODAY = date.today()
+
+
+def date_from_interval(interval):
+    today = date.today()
+    return today - timedelta(weeks=interval)
+
+
+def days_ago(count: int) -> str:
+    if count == 0:
+        return "today"
+
+    suffix = "day" if count == 1 else "days"
+    return f"{count} {suffix} ago"
+
+
+def issue_repo(repository_url):
+    return repository_url.split("/")[-1]
+
+
+# def get_rate_limit(ENDPOINT="/rate_limit") -> List[dict]:
+#     req = requests.get(GITHUB_BASE_URL + ENDPOINT, headers=HEADERS)
+#     if req.status_code == requests.codes.ok:
+#         return req.json()
+
+def pr_is_approved(base_url: str, repo: str, pr_id: int, org: str = "aws-amplify") -> bool:
+    pr_url = f"{base_url}/repos/{org}/{repo}/pulls/{pr_id}/reviews"
+
+    req = requests.get(pr_url, headers=HEADERS)
+
+    is_approved = False
+    for item in req.json():
+        if item["state"] == "APPROVED":
+            is_approved = True
+            break
+
+    return is_approved
+
+
+def pr_id(pr_link):
+    return pr_link.split("/")[-1]
+
+
+def get_issues(ENDPOINT: str = "/search/issues") -> List[dict]:
+
+    issues = []
+    per_page = 100
+    page = 1
+
+    time_interval = date_from_interval(TIME_INTERVAL)
+    created_at = f"created:>{time_interval}"
+
+    filters = (" ").join([f"-label:{label}" for label in FILTER_LABELS])
+
+    params = {"q": f"org:aws-amplify is:open {filters} {created_at}",
+              "per_page": per_page, "page": page}
+
+    req = requests.get(GITHUB_BASE_URL + ENDPOINT,
+                       headers=HEADERS, params=params)
+
+    total_count = req.json()["total_count"]
+    issues = req.json()["items"]
+
+    logging.debug(f"total_count {total_count}, issue count: {len(issues)}")
+
+    if total_count > per_page:
+        logging.debug(f"issue count: {len(issues)}")
+
+        cap = max(total_count, 900)
+        while len(issues) < cap:
+
+            params["page"] = params.get("page", 0) + 1
+            req = requests.get(GITHUB_BASE_URL + ENDPOINT,
+                               headers=HEADERS, params=params)
+
+            if req.json()["items"]:
+                logging.debug('appending...')
+                issues = issues + req.json()["items"]
+
+                logging.debug(
+                    f"issue_count: {len(issues)}, after page:{params.get('page')}")
+
+            else:
+                break
+
+    return issues
+
+
+def truncate_item(item, max_length):
+    if len(item) > max_length:
+        return item[:(max_length - 2)] + ".."
+    return item
+
+
+def get_issue_assignee(issue):
+    assignees = issue.get("assignees", None)
+    if assignees:
+        return",".join([member["login"] for member in assignees])
+
+    assignee = issue.get("assignee", None)
+    if assignee:
+        return assignee["login"]
+
+    return "unassigned"
+
+
+def days_since(issue_date):
+    today = date.today()
+    return (today - datetime.fromisoformat(issue_date[:-1]).date()).days
+
+
+def get_issue_labels(issue):
+    labels = issue.get("labels", None)
+    if labels:
+        return ", ".join([label["name"] for label in labels])
+
+    return ""
+
+
+def is_pr(issue):
+    if issue.get("pull_request", None):
+        return True
+    return False
+
+
+def format_issue(issue):
+    pr = is_pr(issue)
+    id = pr_id(issue["url"])
+    repo = issue_repo(issue['repository_url'])
+    is_approved = False
+
+    if pr:
+        is_approved = pr_is_approved(GITHUB_BASE_URL, repo, id)
+
+    if pr and is_approved:
+        return {}
+
+    return {
+        "repo": repo,
+        "title": truncate_item(issue.get("title"), 50),
+        "is_pr": pr,
+        "is_approved": is_approved,
+        "assignee": get_issue_assignee(issue),
+        "comments": issue.get("comments", 0),
+        "open_since": days_since(issue.get("created_at")),
+        "last_updated": days_since(issue.get("updated_at")),
+        "labels": get_issue_labels(issue),
+        "link": issue["html_url"],
+    }
+
+
+def pr_alerts(issue):
+    # within last 24hrs, follow up and action
+    # FIRST_TIME_CONTRIBUTOR
+    # PR has less than 2 comments
+    # and last comment is from pr author
+    time = ""
+    action = ""
+    unassigned = "👤" if issue["assignee"] == "unassigned" else ""
+    comments = "🔴" if issue['comments'] < 2 else ""
+
+    if issue['last_updated'] > 2 or issue['open_since'] < 1:
+        time = "⏰"
+
+    if issue['last_updated'] < 2 and issue['open_since'] > 7:
+        action = "🤔"
+
+    return f"{time}{unassigned}{comments}{action}"
+
+
+def issue_alerts(issue):
+    action = ""
+    unassigned = "👤" if issue["assignee"] == "unassigned" else ""
+    comments = "🔴" if issue['comments'] == 0 else ""
+    time = "⏰" if issue['last_updated'] > 2 else ""
+
+    if issue['last_updated'] < 2 and issue['open_since'] > 7:
+        action = "🤔"
+
+    return f"{time}{unassigned}{comments}{action}"
+
+
+def format_by_repo(issues):
+    MSG_MAX_LENGTH = 40000
+
+    sorted_issues_by_assignee = sorted(
+        issues, key=lambda k: (k['assignee'], k['is_pr']))
+
+    issues_txt = ""
+    prs_txt = ""
+
+    for issue in sorted_issues_by_assignee:
+        status_length = len(prs_txt + issues_txt)
+        labels = f"({issue['labels']})" if issue['labels'] else ""
+
+        if issue["is_pr"]:
+            pr = f"---\n{pr_alerts(issue)} [{issue['assignee']}] {issue['title']}\n{issue['comments']} comments, created: {days_ago(issue['open_since'])}, updated: {days_ago(issue['last_updated'])} {labels}\n{issue['link']}\n\n"
+            if (len(pr) + status_length) > MSG_MAX_LENGTH:
+                # indicate truncate
+                break
+            else:
+                prs_txt += pr
+        else:
+            issue_rec = f"---\n{issue_alerts(issue)} [{issue['assignee']}] {issue['title']}\n{issue['comments']} comments, created: {days_ago(issue['open_since'])}, updated: {days_ago(issue['last_updated'])} {labels}\n{issue['link']}\n\n"
+            if (len(issue_rec) + status_length) > MSG_MAX_LENGTH:
+                # indicate truncate
+                break
+            else:
+                issues_txt += issue_rec
+
+    return {
+        "prs": prs_txt,
+        "issues": issues_txt
+    }
+
+
+def create_status_reports():
+    repo_ids = [repo["repo"] for repo in repos]
+
+    repo_count = len(repo_ids)
+    if repo_count > 10:
+        logging.warning(
+            "10 requests per minute for searching. Repo count: {repo_count}")
+
+    try:
+        issues = get_issues()
+    except Exception as err:
+        logging.error("Failed to fetch issues. Exiting...")
+        logging.error(err)
+        sys.exit()
+
+    members_by_repo = {repo["repo"]: repo["members"] for repo in repos}
+    webhook_by_repo = {repo["repo"]: repo["webhook"] for repo in repos}
+    name_by_repo = {repo["repo"]: repo["name"] for repo in repos}
+
+    filtered_issues = {repo: [] for repo in repo_ids}
+
+    # one pass to clean up
+    for issue in issues:
+        repo = issue_repo(issue["repository_url"])
+        author = issue["user"]["login"]
+
+        if repo in repo_ids and \
+                author not in members_by_repo[repo]:
+
+            formatted_issue = format_issue(issue)
+            if formatted_issue:
+                filtered_issues[repo].append(formatted_issue)
+
+    status = []
+    meta = {
+        "interval": str(TIME_INTERVAL),
+        "filters": (", ").join(list(FILTER_LABELS)),
+        "date": str(TODAY),
+    }
+
+    for repo_id in repo_ids:
+        issues = filtered_issues[repo_id]
+        if issues:
+            status.append({
+                **{"repo": name_by_repo[repo_id]},
+                **{"webhook": webhook_by_repo[repo_id]},
+                **meta,
+                **format_by_repo(issues)
+            })
+
+    logging.info(f"{len(status)} reports to send.")
+
+    for item in status:
+        try:
+            logging.info(f"Sending webhook for {item['repo']}")
+            req = requests.post(item["webhook"], json=item)
+            logging.info(req)
+        except Exception as err:
+            logging.error("Failed to post webhook. Skipping...")
+            logging.error(err)
+            continue
+
+
+def handler(event, context):
+    create_status_reports()
+
+
+if __name__ == "__main__":
+    create_status_reports()
---
+++ amplify/backend/function/githubdailyupdateteammembers/src/index.py
@@ -0,0 +1,158 @@
+import os
+from datetime import datetime
+from pprint import pprint
+
+import boto3
+import requests
+
+GH_API_TEAM_URL = "https://api.github.com/orgs/aws-amplify/teams/{}/members"
+
+ENV = os.environ.get("ENV", None)
+PROD = ENV == "dev"
+
+
+if PROD:
+    from aws_lambda_powertools import Logger
+    logging = Logger(level="INFO", service="github-updates")
+
+    REGION = os.environ["REGION"]
+    TABLE_NAME = os.environ["STORAGE_DYNAMO41B205C8_NAME"]
+    TOKEN = os.environ["TOKEN"]
+
+    dynamodb = boto3.resource("dynamodb", region_name=REGION)
+    repo_table = dynamodb.Table(TABLE_NAME)
+
+    repos = repo_table.scan()['Items']
+
+
+else:
+    import logging
+    from dotenv import load_dotenv
+
+    logging.basicConfig(format='%(levelname)s:%(message)s',
+                        level=logging.DEBUG)
+
+    load_dotenv()
+    REGION = os.environ.get("REGION")
+    TABLE_NAME = os.environ.get("TABLE_NAME")
+    TOKEN = os.environ.get("TOKEN")
+    WEBHOOK = os.environ.get("WEBHOOK")
+
+    repos = [
+        {
+            "id": "amplify-cli",
+            "repo": "amplify-cli",
+            "team": "amplify-cli",
+            "name": "Amplify CLI"
+        },
+        {
+            "id": "amplify-js",
+            "repo": "amplify-js",
+            "team": "amplify-js",
+            "name": "Amplify JS"
+        },
+        {
+            "id": "amplify-flutter",
+            "repo": "amplify-flutter",
+            "team": "amplify-flutter",
+            "name": "Amplify Flutter"
+        },
+        {
+            "id": "amplify-android",
+            "repo": "amplify-android",
+            "team": "amplify-native",
+            "name": "Amplify Android"
+        },
+        {
+            "id": "amplify-ios",
+            "repo": "amplify-ios",
+            "team": "amplify-native",
+            "name": "Amplify iOS"
+        },
+        {
+            "id": "amplify-codegen",
+            "repo": "amplify-codegen",
+            "team": "amplify-codegen",
+            "name": "Amplify Codegen"
+        },
+        {
+            "id": "amplify-adminui",
+            "repo": "amplify-adminui",
+            "team": "amplify-console",
+            "name": "Amplify Admin UI"
+        },
+        {
+            "id": "amplify-console",
+            "repo": "amplify-console",
+            "team": "amplify-console",
+            "name": "Amplify Console"
+        }
+    ]
+
+
+def get_team_members(id):
+    headers = {"Accept": "application/vnd.github.v3+json",
+               "Authorization": "token " + TOKEN
+               }
+    req = requests.get(GH_API_TEAM_URL.format(id), headers=headers)
+
+    if req.status_code == requests.codes.ok:
+        now = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
+        return {
+            "members": [member["login"] for member in req.json()],
+            "updated_members_at": now
+        }
+
+
+def put_repo_in_ddb(rec, dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb', region_name=REGION)
+
+    table = dynamodb.Table(TABLE_NAME)
+
+    response = table.put_item(Item=rec)
+    return response
+
+
+def update_repo_members(repo_id, team_info, dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb', region_name=REGION)
+
+    table = dynamodb.Table(TABLE_NAME)
+
+    response = table.update_item(
+        Key={"id": repo_id},
+        UpdateExpression="set members=:m, updated_members_at=:u",
+        ExpressionAttributeValues={
+            ':m': team_info["members"],
+            ':u': team_info["updated_members_at"]
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+    return response
+
+
+def init_load_data():
+    for repo in repos:
+        members = get_team_members(repo["team"])
+        webhook = {"webhook": WEBHOOK}
+        repo_info = {**repo, **members, **webhook}
+        resp = put_repo_in_ddb(repo_info)
+        pprint(resp, sort_dicts=False)
+
+
+def handler(event, context):
+    logging.info("run mode: production")
+    for repo in repos:
+        logging.info(f"Updating members for {repo['id']}...")
+        try:
+            team_info = get_team_members(repo["team"])
+            resp = update_repo_members(repo['id'], team_info)
+        except Exception as err:
+            logging.error(err)
+        logging.info(resp)
+
+
+# if __name__ == '__main__':
+
+#     init_load_data()
